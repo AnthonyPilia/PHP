52c1cf3f206dc36f360d36a86a723507afeb70a6
AnthonyPilia <anthony.pilia@laposte.net>
2023-10-26 14:40:12 +0200

    Variables et Concatenations

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+
+// LES VARIABLES
+    $test = "COUCOU";
+
+
+    $chaine = "AU REVOIR";
+
+    $nombre = 3;
+    $tab = array (5,"test");
+
+// Affichage des variables
+    echo $chaine. '<br>';
+    echo $nombre;
+
+    $prenom = "Anthony";
+    $nom = "Pilia";
+
+    echo $prenom;
+    echo $nom;
+
+// LES CONCATENATIONS
+
+    $fullname = $prenom. ' '.$nom;
+    echo $fullname;
+
+// On peut aussi l'afficher ainsi :
+    echo $prenom. ' '.$nom;
+
+    /*
+    Commentaire
+    Sur
+    Plusieurs
+    lignes
+    */
+   
+?>
+
+<!-- msa.camara@gmail.com -->
